902cbc912fed43963fcb385286e2886ebd68ba0e
NodeKas <lukas.frerking.2020@leibniz-fh.de>
2022-03-21 15:19:11 +0100

    Navigation angepasst

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,23 +10,6 @@ class Layout extends React.Component {
                 <header>
                     <h1><span className={"orange"}>Welniz</span> | <span className={"italic"}>Beergame</span></h1>
                 </header>
-                <nav>
-                    <ul>
-                        <NavLink exact={true} to={"/"} activeClassName={"selected"}>
-                            <li>Startseite</li>
-                        </NavLink>
-                        <br />
-                        <NavLink to={"/game/create"} activeClassName={"selected"}>
-                            <li>Neues Spiel</li>
-                        </NavLink>
-                        <NavLink to={"/game/overview"} activeClassName={"selected"}>
-                            <li>Spielübersicht</li>
-                        </NavLink>
-                        <NavLink to={"/game/stats"} activeClassName={"selected"}>
-                            <li>Spielauswertung</li>
-                        </NavLink>
-                    </ul>
-                </nav>
                 <main>
                     { this.props.children }
                 </main>
