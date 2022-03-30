d252ab370583aaa3415e00efa7a2da396baf87ed
melissaschmdt <melissa.schmidt.2020@leibniz-fh.de>
2022-03-30 11:44:47 +0200

    Datenmodell

--- server/functions/Model.js
+++ server/functions/Model.js
@@ -1,18 +1,18 @@
 class Model {
-    constructor(id, gameID, gameCreated, rounds,timePerRound,startValue,raisedValue,roundOfRaise,storage,delay,order,oneRound,twoRound) {
+    constructor(id, gameID, gameCreated, rounds, timePerRound, startStock, startValue, raisedValue, roundOfRaise, storage, delay, order, oneRound, twoRound) {
         this.id = id,
             this.gameID = gameID,
-            this.gameCreated=gameCreated,
-            this.rounds=rounds,
-            this.timePerRound=timePerRound,
-            this.startValue=startValue,
-            this.raisedValue=raisedValue,
-            this.roundOfRaise=roundOfRaise,
-            this.storage=storage,
-            this.delay=delay,
-            this.order=order,
-            this.oneRound=oneRound,
-            this.twoRound=twoRound
-
+            this.gameCreated = gameCreated,
+            this.rounds = rounds,
+            this.timePerRound = timePerRound,
+            this.startStock = startStock,
+            this.startValue = startValue,
+            this.raisedValue = raisedValue,
+            this.roundOfRaise = roundOfRaise,
+            this.storage = storage,
+            this.delay = delay,
+            this.order = order,
+            this.oneRound = oneRound,
+            this.twoRound = twoRound
     }
 }
